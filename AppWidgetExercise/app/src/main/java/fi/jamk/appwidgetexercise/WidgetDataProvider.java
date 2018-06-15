6f10bb43fe3e4e9a256caf55c0a62d002de08572
longnguyendr <long.nguyendr@gmail.com>
2018-06-15 23:21:25 +0300

    newest

---
+++ AppWidgetExercise/app/src/main/java/fi/jamk/appwidgetexercise/WidgetDataProvider.java
@@ -0,0 +1,77 @@
+package fi.jamk.appwidgetexercise;
+
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
+
+    List<String> collection = new ArrayList<>();
+    Context context;
+    Intent intent;
+
+    private void initData() {
+        collection.clear();
+        for (int i = 1; i <=10; i++){
+            collection.add("ListView Item " + i );
+        }
+    }
+
+    public WidgetDataProvider(Context context, Intent intent){
+        this.context = context;
+        this.intent = intent;
+    }
+
+    @Override
+    public void onCreate() {
+        initData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        initData();
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return collection.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                android.R.layout.simple_list_item_1);
+        remoteViews.setTextViewText(android.R.id.text1, collection.get(position));
+        return remoteViews;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
